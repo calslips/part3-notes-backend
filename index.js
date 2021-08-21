a58659132f3183239ffe41210e9719e6d8e300a8
Caleb Phillips <csphillips5@gmail.com>
2021-08-21 13:16:26 -0400

    implement routes to get a single resource & delete resources

--- index.js
+++ index.js
@@ -34,7 +34,21 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   response.json(notes);
-})
+});
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  note ? response.json(note) : (response.status(404).end());
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
 
 const PORT = 3001;
 app.listen(PORT);
