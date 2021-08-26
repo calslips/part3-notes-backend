2202d30a880dc6dbc6a02da85f49630dd4f82fa4
Caleb Phillips <csphillips5@gmail.com>
2021-08-26 17:22:49 -0400

    implement mongoose, fetch all notes from db

--- index.js
+++ index.js
@@ -1,75 +1,92 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const url = require('./.env');
 const app = express();
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method);
-  console.log('Path:', request.path);
-  console.log('Body:', request.body);
-  console.log('---');
+  console.log("Method:", request.method);
+  console.log("Path:", request.path);
+  console.log("Body:", request.body);
+  console.log("---");
   next();
 };
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('build'));
+app.use(express.static("build"));
 app.use(requestLogger);
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useFindAndModify: false,
+  // useCreateIndex: true,
+});
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
 
 let notes = [
   {
     id: 1,
     content: "HTML is easy",
     date: "2019-05-30T17:30:31.098Z",
-    important: true
+    important: true,
   },
   {
     id: 2,
     content: "Browser can execute only Javascript",
     date: "2019-05-30T18:39:34.091Z",
-    important: false
+    important: false,
   },
   {
     id: 3,
     content: "GET and POST are the most important methods of HTTP protocol",
     date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
+    important: true,
+  },
+];
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0;
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
   return maxId + 1;
 };
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes);
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
-app.get('/api/notes/:id', (request, response) => {
+app.get("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   const note = notes.find((note) => note.id === id);
 
-  note ? response.json(note) : (response.status(404).end());
+  note ? response.json(note) : response.status(404).end();
 });
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
 
   response.status(204).end();
 });
 
-app.post('/api/notes', (request, response) => {
+app.post("/api/notes", (request, response) => {
   const body = request.body;
 
   if (!body.content) {
     return response.status(400).json({
-      error: 'content missing'
+      error: "content missing",
     });
   }
 
@@ -77,7 +94,7 @@ app.post('/api/notes', (request, response) => {
     id: generateId(),
     content: body.content,
     date: new Date(),
-    important: body.important || false
+    important: body.important || false,
   };
 
   notes = notes.concat(note);
@@ -86,7 +103,7 @@ app.post('/api/notes', (request, response) => {
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
+  response.status(404).send({ error: "unknown endpoint" });
 };
 
 app.use(unknownEndpoint);
