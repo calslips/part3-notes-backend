756e5b8aa8237f58a3b7b82b9e923db83b0221a3
Caleb Phillips <csphillips5@gmail.com>
2021-08-26 13:22:01 -0400

    revert mongoose to older version for find() to work & retrieve all notes

--- mongo.js
+++ mongo.js
@@ -14,6 +14,8 @@ const url = `mongodb+srv://csp-fso21:${password}@cluster0.tgyrb.mongodb.net/note
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  // useFindAndModify: false,
+  // useCreateIndex: true,
 });
 
 const noteSchema = new mongoose.Schema({
@@ -25,12 +27,66 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
-  content: "HTML is Easy",
+  content: "Figure out why find() is not working",
   date: new Date(),
   important: true,
 });
 
-note.save().then((result) => {
-  console.log("note saved!");
-  mongoose.connection.close();
-});
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+// Note.find({}).exec().then((result) => {
+//   // console.log('Total Find result:', result);
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close()
+// })
+// // .then(
+// //   mongoose.connection.close()
+// // )
+// .catch((err) => {
+//   console.error(err);
+// });
+
+Note.find({})
+  .then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+  // .catch(err => console.log(err))
+
+// Note.find({})
+//   .exec((result) => {
+//     console.log(result)
+//     mongoose.connection.close()
+//   })
+  // .catch(err => console.log('### FIND ERROR:', err))
+
+// const query = Note.find({ important: false })
+// query.then(result => {
+//   console.log(result)
+//   mongoose.connection.close()
+// })
+// console.log(query);
+// mongoose.connection.close()
+
+// .then(result => {
+//   result.forEach(note => {
+//     console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
+
+// Note.find({}, function (err, docs) {
+//   if (err) {
+//     return console.log('### FIND ERROR', err)
+//   }
+//   console.log(docs)
+//   mongoose.connection.close()
+// })
+// .then(mongoose.connection.close());
