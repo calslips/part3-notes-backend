e35d8ab5827bad18b28ecf2b9e86de0f5cfbe04d
Caleb Phillips <csphillips5@gmail.com>
2021-08-27 16:43:09 -0400

    implement middleware for error handling of single resource GET req

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
   // const id = Number(request.params.id);
   // const note = notes.find((note) => note.id === id);
   Note.findById(request.params.id)
@@ -43,10 +43,7 @@ app.get("/api/notes/:id", (request, response) => {
         response.status(404).end();
       }
     })
-    .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: 'malformatted id' });
-    });
+    .catch((error) => next(error));
   // note ? response.json(note) : response.status(404).end();
 });
 
@@ -91,6 +88,18 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
