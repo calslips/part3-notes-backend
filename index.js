97b7c68c1da285877078312aaf11f4b600b3279b
Caleb Phillips <csphillips5@gmail.com>
2021-08-26 20:45:23 -0400

    refactor POST and single resource GET reqs for db use

--- index.js
+++ index.js
@@ -17,27 +17,6 @@ app.use(express.json());
 app.use(express.static("build"));
 app.use(requestLogger);
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
-
 const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
   return maxId + 1;
@@ -54,10 +33,13 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  note ? response.json(note) : response.status(404).end();
+  // const id = Number(request.params.id);
+  // const note = notes.find((note) => note.id === id);
+  Note.findById(request.params.id)
+    .then((note) => {
+      response.json(note);
+    })
+  // note ? response.json(note) : response.status(404).end();
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -76,16 +58,24 @@ app.post("/api/notes", (request, response) => {
     });
   }
 
-  const note = {
-    id: generateId(),
+  // const note = {
+  //   id: generateId(),
+  //   content: body.content,
+  //   date: new Date(),
+  //   important: body.important || false,
+  // };
+  const note = new Note({
     content: body.content,
-    date: new Date(),
     important: body.important || false,
-  };
+    date: new Date(),
+  });
 
-  notes = notes.concat(note);
+  // notes = notes.concat(note);
+  note.save().then((savedNote) => {
+    response.status(201).json(savedNote);
+  });
 
-  response.status(201).json(notes);
+  // response.status(201).json(notes);
 });
 
 const unknownEndpoint = (request, response) => {
