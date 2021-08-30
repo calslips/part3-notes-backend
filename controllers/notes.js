cce83362457cfa7afed6c5ea57efbb4567a43217
Caleb Phillips <csphillips5@gmail.com>
2021-08-30 16:01:35 -0400

    4-1 added delete & put controllers

--- controllers/notes.js
+++ controllers/notes.js
@@ -35,3 +35,28 @@ notesRouter.post('/', (request, response, next) => {
     })
     .catch((error) => next(error));
 });
+
+notesRouter.delete('/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+notesRouter.put('/:id', (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = notesRouter;
