1dc7f59f305408ba3e4587ed3c6412a6a58df8a7
Caleb Phillips <csphillips5@gmail.com>
2021-09-03 13:04:41 -0400

    4-6 optimize beforeEach(), maintain asynchronous functionality

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -9,10 +9,24 @@ const Note = require('../models/note');
 
 beforeEach(async () => {
   await Note.deleteMany({});
-  let noteObject = new Note(helper.initialNotes[0]);
-  await noteObject.save();
-  noteObject = new Note(helper.initialNotes[1]);
-  await noteObject.save();
+
+  // *** for...of to execute promises in specific order
+  for (let note of helper.initialNotes) {
+    let noteObject = new Note(note);
+    await noteObject.save();
+  }
+
+  // *** Promise.all executes promises in parallel
+  // const noteObjects = helper.initialNotes.map((note) => new Note(note));
+  // const promiseArray = noteObjects.map((note) => {
+  //   note.save();
+  // });
+  // await Promise.all(promiseArray);
+
+  // let noteObject = new Note(helper.initialNotes[0]);
+  // await noteObject.save();
+  // noteObject = new Note(helper.initialNotes[1]);
+  // await noteObject.save();
 });
 
 test('notes are returned as json', async () => {
